38425bd9cfb3c1ece2a825aff55a30fcc5dca5b2
liuzhiwei <liuzhiwei@beisen.com>
2018-02-25 15:40:41 +0800

    11111

--- TutoringHome/TutoringHome.Web/Global.asax.cs
+++ TutoringHome/TutoringHome.Web/Global.asax.cs
@@ -16,7 +16,6 @@ namespace TutoringHome.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
